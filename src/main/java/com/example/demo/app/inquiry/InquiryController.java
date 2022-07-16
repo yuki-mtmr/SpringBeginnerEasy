e5dce2c9ea4ddc8942c4d2a0eaa3bbf726a9b6b8
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2022-07-16 10:05:46 +0900

    feat:bootstrap

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -35,13 +35,13 @@ public class InquiryController {
 	public String index(Model model) {
 		List<Inquiry> list = inquiryService.getAll();
 
-		Inquiry inquiry = new Inquiry();
-		inquiry.setId(4);
-		inquiry.setName("Jamie");
-		inquiry.setEmail("sample4@example.com");
-		inquiry.setContents("Hell.");
-
-		inquiryService.update(inquiry);
+//		Inquiry inquiry = new Inquiry();
+//		inquiry.setId(4);
+//		inquiry.setName("Jamie");
+//		inquiry.setEmail("sample4@example.com");
+//		inquiry.setContents("Hell.");
+//
+//		inquiryService.update(inquiry);
 
 //		try {
 //			inquiryService.update(inquiry);
@@ -52,7 +52,7 @@ public class InquiryController {
 
 		model.addAttribute("inquiryList", list);
 		model.addAttribute("title", "Inquiry Index");
-		return "inquiry/index";
+		return "inquiry/index_boot";
 	}
 	
 	@GetMapping("/form")
@@ -60,13 +60,13 @@ public class InquiryController {
 					   Model model,
 					   @ModelAttribute("complete") String complete) {
 		model.addAttribute("title", "Inquiry Form");
-		return "inquiry/form";
+		return "inquiry/form_boot";
 	}
 	
 	@PostMapping("/form")
 	public String formGoBack(InquiryForm inquiryForm, Model model) {
 		model.addAttribute("title", "InquiryForm");
-		return "inquiry/form";
+		return "inquiry/form_boot";
 	}
 	
 	
@@ -76,10 +76,10 @@ public class InquiryController {
 						  Model model) {
 		if (result.hasErrors()) {
 			model.addAttribute("title", "Inquiry Form");
-			return "inquiry/form";
+			return "inquiry/form_boot";
 		}
 		model.addAttribute("title", "Confirm Page");
-		return "inquiry/confirm";
+		return "inquiry/confirm_boot";
 	}
 	
 	@PostMapping("/complete")
@@ -89,7 +89,7 @@ public class InquiryController {
 						   RedirectAttributes redirectAttributes) {
 		if(result.hasErrors()) {
 			model.addAttribute("title", "InquiryForm");
-			return "inquiry/form";
+			return "inquiry/form_boot";
 		}
 
 		Inquiry inquiry = new Inquiry();
@@ -100,7 +100,7 @@ public class InquiryController {
 
 		inquiryService.save(inquiry);
 		redirectAttributes.addFlashAttribute("complete", "Registered");
-		return "redirect:/inquiry/form";
+		return "redirect:/inquiry/form_boot";
 	}
 
 //	@ExceptionHandler(InquiryNotFoundException.class)
